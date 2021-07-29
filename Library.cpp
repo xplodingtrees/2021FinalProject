59b1020a1a1728f135b732fa75922ea384d0ac86
JaguarMother <ocelotmother@gmail.com>
2021-07-28 23:25:49 -0600

    Initial test

---
+++ Library.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ocelo on 7/28/2021.
+//
+
+#include "Library.h"
---
+++ Library.h
@@ -0,0 +1,14 @@
+//
+// Created by ocelo on 7/28/2021.
+//
+
+#ifndef INC_2021FINALPROJECT_LIBRARY_H
+#define INC_2021FINALPROJECT_LIBRARY_H
+
+
+class Library {
+
+};
+
+
+#endif //INC_2021FINALPROJECT_LIBRARY_H
