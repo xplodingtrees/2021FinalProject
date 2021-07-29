aa0dfdd8fbcedf46ee5d83bc3842dec702f46b15
JaguarMother <ocelotmother@gmail.com>
2021-07-29 00:07:38 -0600

    Initial

--- Library.h
+++ Library.h
@@ -1,8 +1,23 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+#include "memory"
+#include "LinkedList.h"
+
+using namespace std;
 
 class Library {
+private:
+    string name;
+    string address;
+    string hours;
+
+public:
+    Library();
+    ~Library();
+
+    bool addBook(Book aBook);
+    LinkedList<Book> availableBooks();
 
 };
 
