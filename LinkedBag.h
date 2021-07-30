324c3b681e82ffa3b100550f858fc0de4bf5a7e7
xplodingtrees <71862215+xplodingtrees@users.noreply.github.com>
2021-07-30 03:21:47 -0600

    added bags, added a patron search, and am om the middle of adding patron relevant methods

---
+++ LinkedBag.h
@@ -0,0 +1,35 @@
+/** ADT bag: Link-based implementation.*/
+
+#ifndef LINKED_BAG_
+#define LINKED_BAG_
+
+#include "BagInterface.h"
+#include "Node.h"
+
+template<class ItemType>
+class LinkedBag : public BagInterface<ItemType>
+{
+private:
+   Node<ItemType>* headPtr; // Pointer to first node
+   int itemCount;           // Current count of bag items
+   
+   // Returns either a pointer to the node containing a given entry
+   // or the null pointer if the entry is not in the bag.
+   Node<ItemType>* getPointerTo(const ItemType& target) const;
+   
+public:
+   LinkedBag();
+   LinkedBag(const LinkedBag<ItemType>& aBag); // Copy constructor
+   virtual ~LinkedBag();                       // Destructor should be virtual
+   int getCurrentSize() const;
+   bool isEmpty() const;
+   bool add(const ItemType& newEntry);
+   bool remove(const ItemType& anEntry);
+   void clear();
+   bool contains(const ItemType& anEntry) const;
+   int getFrequencyOf(const ItemType& anEntry) const;
+   std::vector<ItemType> toVector() const;
+}; // end LinkedBag
+
+#include "LinkedBag.cpp"
+#endif
