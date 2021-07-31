8e2314fe91b0f6f18ef9a31194f2d4d2d2c7d998
JaguarMother <ocelotmother@gmail.com>
2021-07-30 18:07:36 -0600

    node update

--- Node.h
+++ Node.h
@@ -1,22 +1,32 @@
 #ifndef NODE_
 #define NODE_
 
+#include <memory>
+
+using namespace std;
+
 template<class ItemType>
 class Node {
 private:
-    ItemType item;
-    Node<ItemType>* next; //next node in list that we have
+    ItemType item;                     // A data item
+    shared_ptr<Node<ItemType>> next;   // Pointer to next node
 
 public:
-    Node();
-    Node(const ItemType& anItem);
-    Node(const ItemType& anItem, Node<ItemType>* nextNodePtr);
-    void setItem(const ItemType& anItem);
-    void setNext(Node<ItemType>* nextNodePtr);
-    ItemType getItem() const;
-    Node<ItemType>* getNext() const; //returns a node
-};
+    explicit Node();
 
+    explicit Node(const ItemType &anItem);
+
+    Node(const ItemType &anItem, shared_ptr<Node<ItemType>> nextNodePtr);
+
+    void setItem(const ItemType &anItem);
+
+    void setNext(shared_ptr<Node<ItemType>> nextNodePtr);
+
+    ItemType getItem() const;
+
+    shared_ptr<Node<ItemType>> getNext() const;
+
+};
 #include "Node.cpp"
 
-#endif //NODE_
+#endif
