9f9d41092d6d4ecd30755e129ba80a3fc1de310a
JaguarMother <ocelotmother@gmail.com>
2021-07-30 18:02:19 -0600

    node update

--- Node.cpp
+++ Node.cpp
@@ -1,36 +1,40 @@
+
 #include "Node.h"
+#include <cstddef>
 
+/**********************************************************************************************************************/
 template<class ItemType>
-Node<ItemType>::Node() : next(nullptr) {
+Node<ItemType>::Node() {} // end default constructor
 
-}
-
+/**********************************************************************************************************************/
 template<class ItemType>
-Node<ItemType>::Node(const ItemType &anItem) : item(anItem), next(nullptr) {
+Node<ItemType>::Node(const ItemType &anItem) : item(anItem) {} // end constructor
 
-}
-
+/**********************************************************************************************************************/
 template<class ItemType>
-Node<ItemType>::Node(const ItemType &anItem, Node<ItemType> *nextNodePtr) : item(anItem), next(nextNodePtr)  {
+Node<ItemType>::Node(const ItemType &anItem, shared_ptr<Node<ItemType>> nextNodePtr)
+        : item(anItem), next(nextNodePtr) {} // end constructor
 
-}
-
+/**********************************************************************************************************************/
 template<class ItemType>
 void Node<ItemType>::setItem(const ItemType &anItem) {
     item = anItem;
 }
 
+/**********************************************************************************************************************/
 template<class ItemType>
-void Node<ItemType>::setNext(Node<ItemType> *nextNodePtr) {
+void Node<ItemType>::setNext(shared_ptr<Node<ItemType>> nextNodePtr) {
     next = nextNodePtr;
 }
 
+/**********************************************************************************************************************/
 template<class ItemType>
 ItemType Node<ItemType>::getItem() const {
     return item;
 }
 
+/**********************************************************************************************************************/
 template<class ItemType>
-Node<ItemType> *Node<ItemType>::getNext() const {
+shared_ptr<Node<ItemType>> Node<ItemType>::getNext() const {
     return next;
 }
